d8112b376ab8dedb9f035958876788ac9478d68c
John Keith <johnkeith@outlook.com>
2014-06-22 15:13:28 -0400

    add restaurant id column to reviews table

---
+++ db/migrate/20140622191218_add_restaurant_id_to_review.rb
@@ -0,0 +1,5 @@
+class AddRestaurantIdToReview < ActiveRecord::Migration
+  def change
+    add_column :reviews, :restaurant_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140622181142) do
+ActiveRecord::Schema.define(version: 20140622191218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,10 +28,11 @@ ActiveRecord::Schema.define(version: 20140622181142) do
   end
 
   create_table "reviews", force: true do |t|
-    t.integer  "rating",     null: false
-    t.text     "body",       null: false
+    t.integer  "rating",        null: false
+    t.text     "body",          null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "restaurant_id"
   end
 
 end
