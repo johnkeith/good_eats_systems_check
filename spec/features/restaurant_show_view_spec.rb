1e8759fe792a2cd0384d4e727d7af19f210fb729
John Keith <johnkeith@outlook.com>
2014-06-22 15:10:40 -0400

    show view for restaurants; stopping integration test creation

---
+++ spec/features/restaurant_show_view_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+feature "user visits show page for specific restaurant" do
+  scenario "by clicking on link on home page" do
+    populate_restaurants_table
+
+    visit "/"
+    
+    click_link("Restaurant 15")
+    
+    expect(page).to have_content("Information about")
+  end
+
+  # scenario "and sees info for restaurant" do
+  #   populate_restaurants_table
+
+  #   visit '/restaurants/1'
+    
+  #   @restaurant = Restaurant.find(1)
+
+  #   expect(page).to have_content("#{@restaurant.name}")
+  #   expect(page).to have_content("#{@restaurant.description}")
+  #   expect(page).to have_content("#{@restaurant.catagory}")
+  # end
+
+  # scenario "and sees reviews for restaurant" do
+  # end
+end
